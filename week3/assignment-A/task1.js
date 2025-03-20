64dadcc864424c242deb19ccd523acb8b0ed2d7f
ibu063-functional-programming <ibufunctionalprogramming@gmail.com>
2025-03-20 13:16:00 +0100

    Add files via upload

--- week3/assignment-A/task1.js
+++ week3/assignment-A/task1.js
@@ -1,1 +1,42 @@
+// ************ TASK DESCRIPTION ************
 
+// Analyze the given function to determine whether it is pure. 
+// If it is impure, refactor it into a pure function.
+// If it is already pure, retain its original form.
+// In either case you will need to create the function under the "TODO" comment
+
+/*
+*
+*   function sayHello(){
+*       console.log("Hello world");
+*   }
+*
+*/
+
+
+
+// TODO (if needed)
+
+const sayHello = () => "Hello world";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Below this comment any modification is prohibited. 
+module.exports = sayHello;
---
+++ week3/assignment-A/task4.js
@@ -0,0 +1,47 @@
+// ************ TASK DESCRIPTION ************
+
+// Write a PURE function "increaseScore" which updates the score 
+// of a "player" object (1st parameter) by a fixed amount (2nd parameter).
+
+// Example of a player object:
+// * player1 = {"name": "Osman", "score": 30}
+
+
+
+// TODO
+
+const increaseScore = (player, amount) => {
+    return {
+        name: player.name, // Copy existing name
+        score: player.score + amount // Increase score
+    };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Below this comment any modification is prohibited. 
+module.exports = increaseScore;
