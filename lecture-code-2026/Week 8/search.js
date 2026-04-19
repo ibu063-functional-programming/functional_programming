f28366f80b711791d96334a25412ba32d6ec911a
DzelilaMehanovic <dzelila.mehanovic@stu.ibu.edu.ba>
2026-04-19 22:04:35 +0200

    Delete lecture-code-2026/Week 8 directory

--- lecture-code-2026/Week 8/search.js
+++
@@ -1,9 +0,0 @@
-function search(arr, key) {
-  if (arr.length === 0) {
-    return false;
-  } else if (arr[0] === key) {
-    return true;
-  } else {
-    return search(arr.slice(1), key);
-  }
-}
