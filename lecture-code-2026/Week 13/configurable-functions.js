97308b4d8827ba95c6b5723808a553feddf5d10a
DzelilaMehanovic <dzelila.mehanovic@stu.ibu.edu.ba>
2026-05-24 16:30:08 +0200

    Delete lecture-code-2026/Week 13 directory

--- lecture-code-2026/Week 13/configurable-functions.js
+++
@@ -1,7 +0,0 @@
-function makeMultiplier(factor) {
-  return function(x) {
-    return x * factor;
-  };
-}
-const triple = makeMultiplier(3);
-console.assert(triple(5) === 15, "triple function should multiply by 3");
